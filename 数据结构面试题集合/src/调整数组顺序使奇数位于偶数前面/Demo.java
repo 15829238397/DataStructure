45976d666a6b1fd35c7a1e52bc1da11d60475592
pb <15829238397@139.com>
2017-09-06 15:40:27 +0800

    算法改良

--- 数据结构面试题集合/src/调整数组顺序使奇数位于偶数前面/Demo.java
+++ 数据结构面试题集合/src/调整数组顺序使奇数位于偶数前面/Demo.java
@@ -5,7 +5,7 @@ package 调整数组顺序使奇数位于偶数前面;
  * 输入一个整数数组，实现一个函数来调整数组中的数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分。
  *
  */
-public class Demo {
+public class Demo  {
 
 	//思路一：
 	//题目要求我们把所有奇数放在偶数面前，那么我们可以借助两个辅助下标进行性作业。
@@ -51,6 +51,54 @@ public class Demo {
 		return nums ;
 	}
 	
+	//思路二:
+	//我们可以将代码中判断条件(nums[leftIndex] & 0x1) == 1提取出来，这样一来我们就可以完成一些相似的问题，
+	//例如把正数放在复数前面，
+	//把大写字母放在小写字母前面
+	//新建一个接口，将接口里判断的方法交给调用者去实现。
+	public static int[] reorder1(int[] nums , IreOrder<int []> o) {
+		
+		int leftIndex = 0;
+		int rightIndex = 0 ;
+		int temp = 0 ;
+		
+		if(nums == null || nums.length == 0) {
+			return null ;
+		}
+		
+		rightIndex = nums.length - 1 ;
+		
+		while( leftIndex < rightIndex ) {
+			
+			//找到左边第一个偶数
+			while(leftIndex < rightIndex && o.judgeLeft(nums , leftIndex)) {
+				leftIndex ++ ;
+			}
+			
+			//找到右边第一个偶数
+			while(rightIndex > leftIndex && !o.judgeLeft(nums , rightIndex)) {
+				rightIndex -- ;
+			}
+			
+			//两个坐标重合，排序完毕
+			if(leftIndex == rightIndex) {
+				return nums ;
+			}
+			
+			//交换偶数和奇数
+			temp = nums[leftIndex] ;
+			nums[leftIndex] = nums[rightIndex] ;
+			nums[rightIndex] = temp ;
+		
+			leftIndex ++ ;
+			rightIndex -- ;
+			
+		}
+		
+		return nums ;
+	}
+	
+	
 	public static void show(int[] nums) {
 		
 		if(nums == null) {
@@ -76,7 +124,6 @@ public class Demo {
 		int [] nums6 = {2,4,6,8,10} ;
 		int [] nums7 = {1} ;
 		
-		
 		show(reorder(nums1));
 		show(reorder(nums2));
 		show(reorder(nums3));
@@ -85,6 +132,22 @@ public class Demo {
 		show(reorder(nums6));
 		show(reorder(nums7));
 		
+		System.out.println("000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+		
+		IreOrder<int[]> o = (int[] nums , int leftIndex)->{
+			return (nums[leftIndex] & 0x1) == 1 ;
+		} ;
+		
+		show(reorder1(nums1 , o));
+		show(reorder1(nums2 , o));
+		show(reorder1(nums3 , o));
+		show(reorder1(nums4 , o));
+		show(reorder1(nums5 , o));
+		show(reorder1(nums6 , o));
+		show(reorder1(nums7 , o));
+		
 	}
+
+	
 	
 }
